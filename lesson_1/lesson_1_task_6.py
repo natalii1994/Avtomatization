839589c029bd5d3345088ad3e96963ff902926d5
natalii1994 <nata.kirillova1994@gmail.com>
2024-05-09 12:34:22 +0300

    First homework

---
+++ lesson_1/lesson_1_task_6.py
@@ -0,0 +1,41 @@
+def console_1():
+    print( 1 ,end="")
+
+def console_2():
+    print( 2 ,end="")
+
+def console_3():
+    print( 3 ,end="")
+
+def console_4():
+    print( 4 ,end="")
+
+def console_5():
+    print( 5 ,end="")
+
+def console_6():
+    print( 6 ,end="")
+
+def console_7():
+    print( 7 ,end="")
+
+def console_8():
+    print( 8 ,end="")
+
+def console_9():
+    print( 9 ,end="")
+
+def console_0():
+    print( 0 ,end="")
+
+console_8()
+console_8()
+console_0()
+console_0()
+console_5()
+console_5()
+console_5()
+console_3()
+console_5()
+console_3()
+console_5()
