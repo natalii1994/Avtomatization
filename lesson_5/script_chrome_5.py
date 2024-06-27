96b0ad50c2885f4ec453eb2c5d40d913d8b3fa23
Наталья Кириллова <nata.kirillova1994@gmail.com>
2024-06-27 22:55:40 +0300

    Homework5

--- lesson_5/script_chrome_5.py
+++ lesson_5/script_chrome_5.py
@@ -7,4 +7,9 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 #Открыть страницу 
 driver.get("http://the-internet.herokuapp.com/inputs")
-i
+input_locator = driver.find_element(By.CSS_SELECTOR, "input")
+input_locator.send_keys("1000")
+sleep(5)
+input_locator.clear()
+sleep(5)
+input_locator.send_keys("999")
--- lesson_5/script_firefox_3.py
+++ lesson_5/script_firefox_3.py
@@ -2,3 +2,11 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
 
+driver = webdriver.Firefox()
+#Открыть страницу 
+driver.get("http://uitestingplayground.com/classattr")
+#Кликнуть на синюю кнопку 3 раза
+for click in range(3):
+    click_locator = driver.find_element(By.CSS_SELECTOR, "button.btn-primary").click()
+#При выполнении нажатия на синюю кнопку она меняет свое местоположение
+sleep(10)
